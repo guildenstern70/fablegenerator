9f742b7e26e105c7edbf46b7ea67eb00d857537d
Alessio <alessiosaltarin@gmail.com>
2014-01-31 16:52:13 +0100

    Test Batch

---
+++ pdfgenerator/test_all.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+echo Fable #0 :  When I met the Pirates
+CALL pdfgenerator 0 EN M Alessio 30-nov-08
+pause
+CALL pdfgenerator 0 EN F Dana 30-nov-08
+pause
+CALL pdfgenerator 0 RO M Johnny 30-nov-08
+pause
+CALL pdfgenerator 0 RO F Marie 30-nov-08
+pause
+CALL pdfgenerator 0 IT M Alessio 30-nov-08
+pause
+CALL pdfgenerator 0 IT F Diana 30-nov-08
+pause
+echo Fable #1 : My voyage to Aragon
+CALL pdfgenerator 1 EN M Alessio 30-nov-08
+pause
+CALL pdfgenerator 1 EN F Dana 30-nov-08
+pause
+CALL pdfgenerator 1 RO M Johnny 30-nov-08
+pause
+CALL pdfgenerator 1 RO F Marie 30-nov-08
+REM -- NOT YET -- pdfgenerator 1 IT M Alessio 30-nov-08
+REM -- NOT YET -- pause
+REM -- NOT YET -- pdfgenerator 1 IT F Diana 30-nov-08
+CALL pdfgenerator 2 EN M Alessio 30-nov-08
+pause
+CALL pdfgenerator 2 EN F Dana 30-nov-08
+pause
+REM -- NOT YET -- pdfgenerator 2 RO M Johnny 30-nov-08
+REM -- NOT YET -- pause
+REM -- NOT YET -- pdfgenerator 2 RO F Marie 30-nov-08
+pause
+CALL pdfgenerator 2 IT M Alessio 30-nov-08
+pause
+CALL pdfgenerator 2 IT F Diana 30-nov-08
+pause
+echo TEST FINISHED
+
+
+
